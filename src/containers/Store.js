b4a3a42668702dea5758035dabe376b1297b1c35
SmKou <66178856+SmKou@users.noreply.github.com>
2023-09-08 15:32:13 -0700

    Add initial data

---
+++ src/containers/Store.js
@@ -0,0 +1,156 @@
+import { v4 } from 'uuid';
+
+export default function Store() {
+    return [
+        {
+            name: 'Towstump',
+            plant: 'Arabica',
+            origin: 'Ethipia',
+            roast: 'Cinnamon Roast',
+            type: 'Light',
+            quantity: 130,
+            price: 24,
+            id: v4()
+        },
+        {
+            name: 'Gentsia',
+            plant: 'Robusta',
+            origin: 'Vietnam',
+            roast: 'Blonde Roast',
+            type: 'Light',
+            quantity: 130,
+            price: 24,
+            id: v4()
+        },
+        {
+            name: 'Lava',
+            plant: 'Liberica',
+            origin: 'Liberia',
+            roast: 'New England Roast',
+            type: 'Light',
+            quantity: 130,
+            price: 22,
+            id: v4()
+        },
+        {
+            name: 'Caricari',
+            plant: 'Excelsa',
+            origin: 'Vietnam',
+            roast: 'Half City Roast',
+            type: 'Light',
+            quantity: 130,
+            price: 22,
+            id: v4()
+        },
+        {
+            name: 'Illy',
+            plant: 'Arabica',
+            origin: 'India',
+            roast: 'American Roast',
+            type: 'Light-Medium',
+            quantity: 130,
+            price: 20,
+            id: v4()
+        },
+        {
+            name: 'Buckstar',
+            plant: 'Robusta',
+            origin: 'Uganda',
+            roast: 'City Roast',
+            type: 'Medium',
+            quantity: 130,
+            price: 20,
+            id: v4()
+        },
+        {
+            name: 'Teapea',
+            plant: 'Liberica',
+            origin: 'Philippines',
+            roast: 'Breakfast Roast',
+            type: 'Light-Medium',
+            quantity: 130,
+            price: 18,
+            id: v4()
+        },
+        {
+            name: 'K1893',
+            plant: 'Excelsa',
+            origin: 'India',
+            roast: 'Full City Roast',
+            type: 'Medium',
+            quantity: 130,
+            price: 18,
+            id: v4()
+        },
+        {
+            name: 'Equal',
+            plant: 'Arabica',
+            origin: 'Guatemala',
+            roast: 'Continental Roast',
+            type: 'Medium-Dark',
+            quantity: 130,
+            price: 20,
+            id: v4()
+        },
+        {
+            name: 'Belacolom',
+            plant: 'Robusta',
+            origin: 'Dem. Republic of the Congo',
+            roast: 'High Roast',
+            type: 'Medium-Dark',
+            quantity: 130,
+            price: 20,
+            id: v4()
+        },
+        {
+            name: 'Bustelo',
+            plant: 'Arabica & Robusta',
+            origin: 'Brazil',
+            roast: 'New Orleans Roast',
+            type: 'Medium-Dark',
+            quantity: 130,
+            price: 22,
+            id: v4()
+        },
+        {
+            name: 'Kindun',
+            plant: 'Liberica',
+            origin: 'Malaysia',
+            roast: 'Viennese Roast',
+            type: 'Dark',
+            quantity: 130,
+            price: 22,
+            id: v4()
+        },
+        {
+            name: 'Allegro',
+            plant: 'Excelsa',
+            origin: 'Indonesia',
+            roast: 'European Roast',
+            type: 'Dark',
+            quantity: 130,
+            price: 24,
+            id: v4()
+        },
+        {
+            name: 'Community',
+            plant: 'Arabica & Robusta',
+            origin: 'Brazil',
+            roast: 'French Roast',
+            type: 'Dark',
+            quantity: 130,
+            price: 24,
+            id: v4()
+        },
+        {
+            name: 'Death Wish',
+            plant: 'Arabica & Robusta',
+            origin: 'Vietnam',
+            roast: 'Italian Roast',
+            type: 'Dark',
+            quantity: 130,
+            price: 26,
+            id: v4()
+        }
+    ];
+}

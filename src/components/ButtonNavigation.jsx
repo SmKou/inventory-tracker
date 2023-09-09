7924c282b945996f6f386b46840f05f5d6ccd07c
SmKou <66178856+SmKou@users.noreply.github.com>
2023-09-09 04:10:18 -0700

    BUGFIX Add default values and oninput/onchange events to controller form elem

--- src/components/ButtonNavigation.jsx
+++ src/components/ButtonNavigation.jsx
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types'
 import Styles from '../assets/Styles'
 
+const makeKey = str => str.split(" ").join("-") + "-btn"
+
 function ButtonNavigation(props) {
     return <div style={ Styles.btnNavigation.div }>
-        {props.buttons.map((btn) => <button style={Styles.btnNavigation.button} onClick={btn.handleClick}>{btn.buttonText}</button>)}
+        {props.buttons.map((btn) => <button style={Styles.btnNavigation.button} onClick={btn.handleClick} key={makeKey(btn.buttonText)}>{btn.buttonText}</button>)}
     </div>
 }
 
--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -1,25 +1,25 @@
+import { useState } from 'react'
 import PropTypes from 'prop-types'
 import { v4 } from 'uuid'
 import Store from '../containers/Store'
 import ButtonNavigation from './ButtonNavigation'
 
 function ProductForm(props) {
+    const product = props.product;
+    const [name, setName] = useState(product ? product.name : '')
+    const [plant, setPlant] = useState(product ? product.plant : '')
+    const [origin, setOrigin] = useState(product ? product.origin : '')
+    const [roast, setRoast] = useState(product ? product.roast : '')
+    const [type, setType] = useState(product ? product.type : '')
+    const [quantity, setQty] = useState(product ? product.quantity : 130)
+    const [price, setPrice] = useState(product ? product.price : 1)
+    const id = product ? product.id : ''
+
     const handleSubmit = e => {
         e.preventDefault();
-        const data = new FormData(e.target);
-        const product = {
-            name: data.get("name"),
-            plant: data.get("plant"),
-            origin: data.get("origin"),
-            roast: data.get("roast"),
-            type: data.get("type"),
-            quantity: data.get("quantity"),
-            price: data.get("price"),
-            id: data.get("id") || v4()
-        };
+        const product = { name, plant, origin, roast, type, quantity, price, id: id || v4() };
         props.onFormSubmit(product)
     }
-    const product = props.product;
 
     return <section>
         <ButtonNavigation buttons={[{
@@ -27,40 +27,40 @@ function ProductForm(props) {
             handleClick: props.goToList
         }]} />
         <form onSubmit={handleSubmit}>
-            <input type="hidden" name="id" value={product ? product.id : ''} />
+            <input type="hidden" name="id" value={id} />
             <label>
                 Product Name:
-                <input name="name" value={product ? product.name : ''} />
+                <input name="name" value={name} onInput={e => setName(e.target.value)} />
             </label>
             <label>
                 Plant Species:
-                <select name="plant" value={product ? product.plant : ''}>
+                <select name="plant" value={plant} onChange={e => setPlant(e.target.value)}>
                     {Store.plant.map((plant) => <option value={plant}>{plant}</option>)}
                 </select>
             </label>
             <label>
                 Country of Origin:
-                <input name="origin" value={product ? product.origin : ''} />
+                <input name="origin" value={origin} onInput={e => setOrigin(e.target.value)} />
             </label>
             <label>
                 Roast:
-                <select name="roast" value={product ? product.roast : ''}>
+                <select name="roast" value={roast} onChange={e => setRoast(e.target.value)}>
                     {Store.roast.map((roast) => <option value={roast}>{roast}</option>)}
                 </select>
             </label>
             <label>
                 Type:
-                <select name="type" value={product ? product.type : ''}>
+                <select name="type" value={type} onChange={e => setType(e.target.value)}>
                     {Store.type.map((type) => <option value={type}>{type}</option>)}
                 </select>
             </label>
             <label>
                 Quantity:
-                <input type="number" name="quantity" value={product ? product.quantity : 130} />
+                <input type="number" name="quantity" value={quantity} onInput={e => setQty(e.target.value)} />
             </label>
             <label>
                 Price:
-                <input type="number" name="price" value={product ? product.price : 1} />
+                <input type="number" name="price" value={price} onInput={e => setPrice(e.target.value)} />
             </label>
             <input type="submit" value={props.submitText} />
         </form>
